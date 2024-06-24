40633fca81f1e35bc3012f95e2ba9de146f52f88
Han Ju Kim <hjkim2902@gmail.com>
2024-06-24 17:06:54 +0900

    first commit

---
+++ src/test/java/ai/greenmate/greenmate_backend/GreenmateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.greenmate.greenmate_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenmateBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
