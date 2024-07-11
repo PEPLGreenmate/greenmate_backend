f433e6409e13001b182b81e6891164ba781dc7b5
Han Ju Kim <hjkim2902@gmail.com>
2024-07-11 14:30:55 +0900

    feat : Member Entity & Repository 추가

---
+++ src/main/java/ai/greenmate/greenmate_backend/domain/member/entity/Gender.java
@@ -0,0 +1,5 @@
+package ai.greenmate.greenmate_backend.domain.member.entity;
+
+public enum Gender {
+  MALE, FEMALE, OTHER;
+}
--- src/main/java/ai/greenmate/greenmate_backend/domain/member/entity/Member.java
+++ src/main/java/ai/greenmate/greenmate_backend/domain/member/entity/Member.java
@@ -1,4 +1,41 @@
 package ai.greenmate.greenmate_backend.domain.member.entity;
 
-public class Member {
+import ai.greenmate.greenmate_backend.global.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Member extends BaseEntity {
+
+  @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "member_id")
+  private long id;
+  private String email;
+  private String password;
+  private String nickname;
+  @Enumerated(value = EnumType.STRING)
+  private Gender gender;
+  private String address;
+  private LocalDate birthday;
+  private int feed;
+  private int interaction;
+  private int depressIndex;
+  private int stressIndex;
+  private int anxietyIndex;
+  private LocalDateTime depressTestAt;
+  private LocalDateTime stressTestAt;
+  private LocalDateTime anxietyTestAt;
 }
