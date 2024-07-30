2b217e87dfd3602cbf264ade6be8c20ce2150fa6
Han Ju Kim <hjkim2902@gmail.com>
2024-07-30 18:13:16 +0900

    fix : EmotionMarble 엔티티 수정

--- src/main/java/ai/greenmate/greenmate_backend/domain/emotionmarble/entity/EmotionMarble.java
+++ src/main/java/ai/greenmate/greenmate_backend/domain/emotionmarble/entity/EmotionMarble.java
@@ -1,6 +1,7 @@
 package ai.greenmate.greenmate_backend.domain.emotionmarble.entity;
 
 import ai.greenmate.greenmate_backend.domain.member.entity.Member;
+import ai.greenmate.greenmate_backend.domain.mission.entity.Mission;
 import ai.greenmate.greenmate_backend.global.entity.BaseEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -9,6 +10,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
@@ -20,10 +22,14 @@ public class EmotionMarble extends BaseEntity {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
 
-  @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "member_id")
-  private Member member;
   private String title;
   private String emotion; // TODO : Enum Class에 담을지?
 
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "member_id")
+  private Member member;
+
+  @OneToOne
+  @JoinColumn(name = "mission_id")
+  private Mission mission;
 }
