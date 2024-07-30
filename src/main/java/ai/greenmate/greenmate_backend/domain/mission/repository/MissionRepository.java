49c32918e9b8a17be7a696309434aa583a8ee1df
Han Ju Kim <hjkim2902@gmail.com>
2024-07-30 18:13:38 +0900

    feat : Mission API 생성

--- src/main/java/ai/greenmate/greenmate_backend/domain/mission/repository/MissionRepository.java
+++ src/main/java/ai/greenmate/greenmate_backend/domain/mission/repository/MissionRepository.java
@@ -1,9 +1,13 @@
 package ai.greenmate.greenmate_backend.domain.mission.repository;
 
+import ai.greenmate.greenmate_backend.domain.greenmate.entity.Greenmate;
 import ai.greenmate.greenmate_backend.domain.mission.entity.Mission;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MissionRepository extends JpaRepository<Mission, Long> {
+  List<Mission> findByGreenmate(Greenmate greenmate);
 }
