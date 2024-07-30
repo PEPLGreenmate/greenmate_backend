c5d14c90888061c6f87cdebc3563ea07ac84f08f
Han Ju Kim <hjkim2902@gmail.com>
2024-07-30 18:13:49 +0900

    fix : 서버 에러 수정

--- src/main/java/ai/greenmate/greenmate_backend/global/exception/handler/GreenmateControllerAdvice.java
+++ src/main/java/ai/greenmate/greenmate_backend/global/exception/handler/GreenmateControllerAdvice.java
@@ -1,9 +1,11 @@
 package ai.greenmate.greenmate_backend.global.exception.handler;
 
 import ai.greenmate.greenmate_backend.global.dto.BaseResponse;
+import ai.greenmate.greenmate_backend.global.dto.BaseResponseStatus;
 import ai.greenmate.greenmate_backend.global.exception.GreenmateException;
 import ai.greenmate.greenmate_backend.global.exception.exception.AuthException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -28,7 +30,7 @@ public class GreenmateControllerAdvice {
   @ExceptionHandler(value = Exception.class)
   public ResponseEntity<BaseResponse> handleException(Exception e){
     log.info("handleException", e);
-    return ResponseEntity.badRequest()
-            .body(new BaseResponse<>(e));
+    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body(new BaseResponse<>(BaseResponseStatus.INTERNAL_SERVER_ERROR));
   }
 }
