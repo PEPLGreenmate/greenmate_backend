7dbde7bdb8320556a1f88fd8fb688df1441542dc
Han Ju Kim <hjkim2902@gmail.com>
2024-07-30 18:09:48 +0900

    feat : 500 서버 에러 처리

--- src/main/java/ai/greenmate/greenmate_backend/global/dto/BaseResponseStatus.java
+++ src/main/java/ai/greenmate/greenmate_backend/global/dto/BaseResponseStatus.java
@@ -11,6 +11,7 @@ public enum BaseResponseStatus {
   UNAUTHORIZED(false, HttpStatus.UNAUTHORIZED, 401, "인증이 필요합니다."),
   FORBIDDEN(false, HttpStatus.FORBIDDEN, 403, "권한이 없습니다."),
   NOT_FOUND(false, HttpStatus.NOT_FOUND, 404, "대상을 찾을 수 없습니다."),
+  INTERNAL_SERVER_ERROR(false, HttpStatus.INTERNAL_SERVER_ERROR, 500, "서버 내부에 오류가 발생했습니다."),
 
   OVER_DAILY_TRY_COUNT(false, HttpStatus.BAD_REQUEST, 4001, "인증 하루 시도 횟수를 초과하였습니다."),
   NOT_VALID_CODE(false, HttpStatus.BAD_REQUEST, 4002, "잘못된 코드입니다."),
