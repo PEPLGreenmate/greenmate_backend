1c636797f4536b1d93877aaf2a1aee7e558f6a90
Han Ju Kim <hjkim2902@gmail.com>
2024-08-02 17:48:41 +0900

    feat : 다이어리 API 로직 구현

--- src/main/java/ai/greenmate/greenmate_backend/domain/diary/repository/DiaryRepository.java
+++ src/main/java/ai/greenmate/greenmate_backend/domain/diary/repository/DiaryRepository.java
@@ -1,9 +1,13 @@
 package ai.greenmate.greenmate_backend.domain.diary.repository;
 
 import ai.greenmate.greenmate_backend.domain.diary.entity.Diary;
+import ai.greenmate.greenmate_backend.domain.greenmate.entity.Greenmate;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
+  List<Diary> findByGreenmate(Greenmate greenmate);
 }
---
+++ src/main/java/ai/greenmate/greenmate_backend/domain/diary/service/DiaryService.java
@@ -0,0 +1,45 @@
+package ai.greenmate.greenmate_backend.domain.diary.service;
+
+import ai.greenmate.greenmate_backend.domain.diary.dto.DiaryDTO;
+import ai.greenmate.greenmate_backend.domain.diary.dto.GetDiariesResponse;
+import ai.greenmate.greenmate_backend.domain.diary.repository.DiaryRepository;
+import ai.greenmate.greenmate_backend.domain.greenmate.entity.Greenmate;
+import ai.greenmate.greenmate_backend.domain.greenmate.repository.GreenmateRepository;
+import ai.greenmate.greenmate_backend.domain.member.entity.Member;
+import ai.greenmate.greenmate_backend.domain.member.repository.MemberRepository;
+import ai.greenmate.greenmate_backend.global.dto.BaseResponseStatus;
+import ai.greenmate.greenmate_backend.global.exception.GreenmateException;
+import ai.greenmate.greenmate_backend.global.jwt.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class DiaryService {
+  private final MemberRepository memberRepository;
+  private final DiaryRepository diaryRepository;
+  private final JwtService jwtService;
+  private final GreenmateRepository greenmateRepository;
+
+  public GetDiariesResponse getAllDiaries() {
+    String email = jwtService.getEmail();
+    Member member = memberRepository.findByEmail(email)
+            .orElseThrow(() -> new GreenmateException(BaseResponseStatus.NOT_VALID_EMAIL));
+    Greenmate greenmate = greenmateRepository.findByMember(member)
+            .orElseThrow(() -> new GreenmateException(BaseResponseStatus.NOT_FOUND));
+    List<DiaryDTO> diaryDTOs = diaryRepository.findByGreenmate(greenmate)
+            .stream()
+            .sorted((x, y) -> {
+              if (x.getCreatedAt().isAfter(y.getCreatedAt())) return 1;
+              else return -1;
+            })
+            .map(diary -> new DiaryDTO(diary.getId(), diary.getCreatedAt().toLocalDate(), diary.getContent()))
+            .toList();
+    return new GetDiariesResponse(diaryDTOs);
+
+  }
+}
