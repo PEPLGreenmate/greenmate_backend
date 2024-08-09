a20ef629f5bd459dc7c69853462c917908e19b89
Han Ju Kim <hjkim2902@gmail.com>
2024-08-09 22:25:26 +0900

    feat : Diary 조회 API 추가

--- src/main/java/ai/greenmate/greenmate_backend/domain/diary/controller/DiaryController.java
+++ src/main/java/ai/greenmate/greenmate_backend/domain/diary/controller/DiaryController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/api/diaries")
@@ -16,8 +17,8 @@ public class DiaryController {
   private final DiaryService diaryService;
 
   @GetMapping
-  public ResponseEntity<BaseResponse> getDiaries(){
-    GetDiariesResponse getDiariesResponse = diaryService.getAllDiaries();
+  public ResponseEntity<BaseResponse> getDiaries(@RequestParam("greenmateId") String greenmateId){
+    GetDiariesResponse getDiariesResponse = diaryService.getAllDiaries(greenmateId);
     return ResponseEntity.ok(new BaseResponse(getDiariesResponse));
   }
 }
